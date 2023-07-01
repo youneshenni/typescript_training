5d30c95b19df4656d4319b052466d889ee1e9e27
younes <youn.henni@icloud.com>
2023-07-01 15:48:16 +0100

    Adds final typing

--- training1/scripts/index.ts
+++ training1/scripts/index.ts
@@ -1,14 +1,30 @@
-import * as addToDatabase from './script1.js';
-import * as removeFromDatabase from './script2.js';
-import defaultDatabase from './database.js';
+import * as addToDatabase from "./script1.js";
+import * as removeFromDatabase from "./script2.js";
+import defaultDatabase from "./database.js";
 let database = defaultDatabase;
 
-const scriptWrapper = func => (...params) => {
+const scriptWrapper =
+  (func) =>
+  (...params) => {
     database = func(database, ...params);
     return database;
+  };
+
+const scripts = {
+  addToDatabase: addToDatabase.default,
+  removeFromDatabase: removeFromDatabase.default,
 };
 
-const scripts = {addToDatabase:addToDatabase.default, removeFromDatabase:removeFromDatabase.default};
+type OmitFirstArg<F> = F extends (
+  x: typeof database,
+  ...args: infer P
+) => infer R
+  ? (...args: P) => R
+  : never;
 
-export default Object.keys(scripts).reduce((acc, cur) => ({...acc, [cur]: scriptWrapper(scripts[cur])}), {} as {[key in keyof typeof scripts]: ReturnType<typeof scriptWrapper>});
-
+export default Object.keys(scripts).reduce(
+  (acc, cur) => ({ ...acc, [cur]: scriptWrapper(scripts[cur]) }),
+  {} as {
+    [key in keyof typeof scripts]: OmitFirstArg<(typeof scripts)[key]>;
+  }
+);
